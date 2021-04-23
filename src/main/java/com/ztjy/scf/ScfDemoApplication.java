8d48b4ce0bd892f3e37d80008f2985e83ac34026
lindi <lindi@szy.cn>
2021-04-23 13:43:20 +0800

    去除springboot function

--- src/main/java/com/ztjy/scf/ScfDemoApplication.java
+++ src/main/java/com/ztjy/scf/ScfDemoApplication.java
@@ -2,22 +2,12 @@ package com.ztjy.scf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.Function;
 
 @SpringBootApplication
 public class ScfDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ScfDemoApplication.class, args);
-    }
-
-    @Bean
-    public Function<String, String> uppercase() {
-        return value -> value.toUpperCase();
     }
 
 }
